ae9233b79ff7c639010f5b3cb7af412e64549a7e
Leena Domadia <leena.domadia2018@gmail.com>
2021-08-17 22:33:57 -0400

    adding actions and reducers

--- src/store.js
+++ src/store.js
@@ -1,6 +1,9 @@
 import { createStore, combineReducers } from 'redux';
+import { todos } from './todos/reducers.js';
 
-const reducers = {};
+const reducers = {
+    todos
+};
 
 const rootReducer = combineReducers(reducers);
 
