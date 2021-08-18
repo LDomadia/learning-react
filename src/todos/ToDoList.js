8b61189b063475cec65d503de2b1f95492af948b
Leena Domadia <leena.domadia2018@gmail.com>
2021-08-17 21:33:19 -0400

    styling fixes

--- src/todos/ToDoList.js
+++ src/todos/ToDoList.js
@@ -4,7 +4,7 @@ import NewToDoForm from './NewToDoForm';
 
 import './ToDoList.css';
 
-const ToDoList = ({ todos = [{ text: "No"}] }) => (
+const ToDoList = ({ todos = [{ text: "No"}, { text: "Hello my name is Leena Domadia and I am short and stout and have dark skin and black hair and I'm strange, Dr. Strange"}] }) => (
     <div className="list-wrapper">
         <NewToDoForm />
         {todos.map(todo => <ToDoListItem todo={todo} />)}
